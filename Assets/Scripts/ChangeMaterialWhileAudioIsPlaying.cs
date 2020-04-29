5ac549ffad236a267e17f7e6960be615002a2f4f
dominikheller <dheller@smail.uni-koeln.de>
2020-04-30 01:51:21 +0200

    made a button that plays audio and doesn't glitch, made a launchpad with it
    VRTK interactables sind standardmäßig super glitchy, sehr viele tweaks notwendig damit es ordentlich funktioniert

---
+++ Assets/Scripts/ChangeMaterialWhileAudioIsPlaying.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ChangeMaterialWhileAudioIsPlaying : MonoBehaviour
+{
+    
+    public GameObject gameObject;
+    public GameObject gameObject2;
+    public AudioSource audioSource;
+    public Material materialBefore;
+    public Material materialDuring;
+    public Material materialNone;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (audioSource.clip == null)
+        {
+            Renderer rend = gameObject.GetComponent<Renderer>();
+            rend.material = materialNone;
+            Renderer rend2 = gameObject2.GetComponent<Renderer>();
+            rend2.material = materialNone;
+       
+        }
+
+        else if (audioSource.isPlaying)
+        {
+            Renderer rend = gameObject.GetComponent<Renderer>();
+            rend.material = materialDuring;
+            Renderer rend2 = gameObject2.GetComponent<Renderer>();
+            rend2.material = materialDuring;
+
+        }
+
+        else
+        {
+            Renderer rend = gameObject.GetComponent<Renderer>();
+            rend.material = materialBefore;
+            Renderer rend2 = gameObject2.GetComponent<Renderer>();
+            rend2.material = materialBefore;
+        }
+    }
+}
