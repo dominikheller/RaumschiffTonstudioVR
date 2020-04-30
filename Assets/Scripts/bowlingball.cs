65c2402d034e7dc42f70bf3951d40ed4f0e5ca60
dominikheller <dheller@smail.uni-koeln.de>
2020-04-30 20:25:50 +0200

    made a small bowling alley to play around with physics

---
+++ Assets/Scripts/bowlingball.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bowlingball : MonoBehaviour
+
+{
+
+    public Vector3 ballStartingLocation;
+    public GameObject ball;
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void resetBowlingBall()
+    {
+
+        ball.transform.position = ballStartingLocation;
+        ball.transform.rotation = Quaternion.identity;
+        rb.velocity = new Vector3(0.0f, 0.0f, 0.0f);
+        rb.angularVelocity = new Vector3(0.0f, 0.0f, 0.0f);
+
+    }
+}
