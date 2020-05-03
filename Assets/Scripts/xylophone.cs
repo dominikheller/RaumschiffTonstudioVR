ce6d811757ff9323b25414f79ca55433d9b1415d
dominikheller <dheller@smail.uni-koeln.de>
2020-05-03 13:36:04 +0200

    made a functioning Xylophone as well as a scale- and grabable hologram version

---
+++ Assets/Scripts/xylophone.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class xylophone : MonoBehaviour
+{
+
+    public AudioSource audioData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        audioData.Play(0);
+    }
+
+
+}
