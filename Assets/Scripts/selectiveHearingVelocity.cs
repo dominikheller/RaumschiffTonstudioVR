e4fde04f745a795e565421d41ad41e44e5456dd6
dominikheller <dheller@smail.uni-koeln.de>
2020-05-04 19:19:47 +0200

    changed bouncing sound sources to be zero-g and added initial velocity to achive a constant speed

---
+++ Assets/Scripts/selectiveHearingVelocity.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class selectiveHearingVelocity : MonoBehaviour
+{
+
+    public Rigidbody rigidbody;
+    public Vector3 velocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody.velocity = velocity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
