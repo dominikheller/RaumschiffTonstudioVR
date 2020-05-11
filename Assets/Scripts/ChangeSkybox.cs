15aa72156c270aeb6e591c50fc3ea19cb245d3a9
dominikheller <dheller@smail.uni-koeln.de>
2020-05-11 23:04:14 +0200

    added some buttons to change the skybox ingame

---
+++ Assets/Scripts/ChangeSkybox.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSkybox : MonoBehaviour
+{
+
+    public Material skyboxToChangeTo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeTheSkybox()
+    {
+
+        RenderSettings.skybox = skyboxToChangeTo;
+
+    }
+}
