42ce88377390ebcbc893b299e7467653ba456e52
malbero <34505168+malbero@users.noreply.github.com>
2020-05-15 15:59:48 +0200

    Add audio box

---
+++ Assets/Scripts/AudioBox.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+
+public class AudioBox : MonoBehaviour
+{
+    public GameObject controller;
+    public AudioClip[] audioClips;
+
+    private int audioBoxSize = 1;
+    private Vector3 negativeAxisOffset = new Vector3(-.5f, -.5f, -.5f);
+    private int numberOfClips;
+    private int numberOfFieldsPerAxis;
+    private float soundFieldSize;
+
+    private AudioClip[,] twoDimensionalArray;
+    private int xIndex;
+    private int yIndex;
+    private int zIndex;
+
+    void Awake()
+    {
+        numberOfClips = audioClips.Length;
+        numberOfFieldsPerAxis = (int)Mathf.Sqrt(numberOfClips);
+        soundFieldSize = audioBoxSize / Mathf.Sqrt(numberOfClips);
+
+        mapAudioClipsToTwoDimensionalArray();
+    }
+
+    void Update()
+    {
+        checkXValues();
+        checkYValues();
+        checkZValues();
+    }
+
+    void mapAudioClipsToTwoDimensionalArray()
+    {
+        twoDimensionalArray = new AudioClip[numberOfFieldsPerAxis, numberOfFieldsPerAxis];
+        for (int i = 0; i < numberOfClips; i++)
+        {
+            twoDimensionalArray[0, 0] = audioClips[0];
+            twoDimensionalArray[0, 1] = audioClips[1];
+            twoDimensionalArray[0, 2] = audioClips[2];
+            twoDimensionalArray[1, 0] = audioClips[3];
+            twoDimensionalArray[1, 1] = audioClips[4];
+            twoDimensionalArray[1, 2] = audioClips[5];
+            twoDimensionalArray[2, 0] = audioClips[6];
+            twoDimensionalArray[2, 1] = audioClips[7];
+            twoDimensionalArray[2, 2] = audioClips[8];
+        }
+    }
+
+    void checkXValues()
+    {
+        for (int i = 0; i < numberOfFieldsPerAxis; i++)
+        {
+            if (controller.transform.localPosition.x >= negativeAxisOffset.x + soundFieldSize * i && controller.transform.localPosition.x <= negativeAxisOffset.x + soundFieldSize * (i + 1))
+            {
+                if (xIndex != i)
+                {
+                    xIndex = i;
+                    setAudioClip();
+                }
+            }
+        }
+    }
+
+    void checkYValues()
+    {
+        for (int i = 0; i < numberOfFieldsPerAxis; i++)
+        {
+            if (controller.transform.localPosition.y >= negativeAxisOffset.y + soundFieldSize * i && controller.transform.localPosition.y <= negativeAxisOffset.y + soundFieldSize * (i + 1))
+            {
+                if (yIndex != i)
+                {
+                    yIndex = i;
+                    setAudioClip();
+                }
+            }
+        }
+    }
+
+    void checkZValues()
+    {
+        for (int i = 0; i < numberOfFieldsPerAxis; i++)
+        {
+            if (controller.transform.localPosition.z >= negativeAxisOffset.z + soundFieldSize * i && controller.transform.localPosition.z <= negativeAxisOffset.z + soundFieldSize * (i + 1))
+            {
+                if (zIndex != i)
+                {
+                    zIndex = i;
+                    setPitchLevel();
+                }
+            }
+        }
+    }
+
+    void setAudioClip()
+    {
+        gameObject.GetComponent<AudioSource>().clip = twoDimensionalArray[xIndex, yIndex];
+        gameObject.GetComponent<AudioSource>().Play();
+    }
+
+    void setPitchLevel()
+    {
+        switch (zIndex)
+        {
+            case 0:
+                gameObject.GetComponent<AudioSource>().pitch -= .5f;
+                gameObject.GetComponent<AudioSource>().Play();
+                break;
+            case 1:
+                gameObject.GetComponent<AudioSource>().pitch = 1;
+                gameObject.GetComponent<AudioSource>().Play();
+                break;
+            case 2:
+                gameObject.GetComponent<AudioSource>().pitch += .5f;
+                gameObject.GetComponent<AudioSource>().Play();
+                break;
+            default:
+                gameObject.GetComponent<AudioSource>().pitch = 1;
+                gameObject.GetComponent<AudioSource>().Play();
+                break;
+        }
+    }
+}
+
