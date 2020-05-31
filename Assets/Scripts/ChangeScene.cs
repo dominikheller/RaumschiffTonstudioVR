41e7a236077aff9a82818fd6ca64cbc99a5f8af4
malbero <34505168+malbero@users.noreply.github.com>
2020-05-31 13:30:19 +0200

    Pre-finalize scene transitions

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -13,11 +13,26 @@ public class ChangeScene : MonoBehaviour
 
     public void changeSceneAdditive(string sceneName)
     {
+        Scene activeScene = SceneManager.GetActiveScene();
+        setGameObjectsStatus(activeScene.GetRootGameObjects(), false);
         SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName(sceneName));
     }
 
     public void unloadScene()
     {
         SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().name);
+        Scene rootScene = SceneManager.GetSceneAt(0);
+        SceneManager.SetActiveScene(rootScene);
+        setGameObjectsStatus(rootScene.GetRootGameObjects(), true);
+    }
+
+    public void setGameObjectsStatus(GameObject[] objects, bool status)
+    {
+        for (int i = 0; i < objects.Length; i++)
+        {
+            objects[i].gameObject.SetActive(status);
+        }
     }
 }
+
---
+++ Assets/Scripts/GlobalPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class GlobalPlayer : MonoBehaviour
+{
+    public static GlobalPlayer Instance;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
