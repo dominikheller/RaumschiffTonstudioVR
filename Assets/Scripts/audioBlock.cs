faa36a72448fdcfd213543b59b7a76a546b76b9b
malbero <34505168+malbero@users.noreply.github.com>
2020-06-04 19:41:55 +0200

    Add progress bar prototype for audio blocks

--- Assets/Scripts/audioBlock.cs
+++ Assets/Scripts/audioBlock.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 
 public class audioBlock : MonoBehaviour
 {
-    
     public GameObject gameObject;
     public AudioSource audioSource;
     public Material materialBefore;
@@ -16,38 +15,44 @@ public class audioBlock : MonoBehaviour
     public bool activated = false;
     Vector3 blockSize;
 
+    public GameObject progressBar;
+    private float timer;
+    private float unitLength;
+    private float unitTime;
+
     void Start()
     {
-
         blockSize = transform.localScale;
         blockSize.x += audioSource.clip.length * 0.2f;
         transform.localScale = blockSize;
 
+        unitTime = audioSource.clip.length / 100;
+        unitLength = gameObject.transform.localScale.z / 100;
+
+        Debug.Log("audioLength: " + audioSource.clip.length);
+        Debug.Log("gameObjectLength: " + gameObject.transform.localScale.z);
+        Debug.Log("unitTime: " + unitTime);
+        Debug.Log("unitLength: " + unitLength);
     }
 
     void Update()
     {
+        timer += Time.deltaTime;
+
         if (audioSource.clip == null)
         {
             Renderer rend = gameObject.GetComponent<Renderer>();
             rend.material = materialNone;
-            
-       
         }
 
         else if (audioSource.isPlaying)
         {
             Renderer rend = gameObject.GetComponent<Renderer>();
             rend.material = materialDuring;
-            
-
-        }
-
-        else
+        } else
         {
             Renderer rend = gameObject.GetComponent<Renderer>();
-            rend.material = materialBefore;
-            
+            rend.material = materialBefore;   
         }
 
         if (activated && !audioSource.isPlaying)
@@ -55,16 +60,23 @@ public class audioBlock : MonoBehaviour
             shootRaycastForNext();
         }
 
-
+        if(activated && timer >= unitTime && progressBar.transform.localScale.z <= gameObject.transform.localScale.z)
+        {
+            if (progressBar != null)
+            {
+                progressBar.transform.localScale += new Vector3(0, 0, unitLength);
+            }
+        }
     }
 
 
     public void shootRaycastForNext()
     {
-
         RaycastHit hit;
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.right), out hit))
+        {
             hit.transform.SendMessage("triggered");
+        }
 
         activated = false;
     }
@@ -76,11 +88,5 @@ public class audioBlock : MonoBehaviour
         audioSource.Play(0);
         Renderer rend = gameObject.GetComponent<Renderer>();
         rend.material = materialNone;
-        
     }
-
-    
-
-    
-
 }
