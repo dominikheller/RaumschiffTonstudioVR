941b43e0e64512aedf469d6ba565962a720e659f
malbero <34505168+malbero@users.noreply.github.com>
2020-06-07 20:06:26 +0200

    Add prototypical drumset

---
+++ Assets/Scripts/Drums.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drums : MonoBehaviour
+{
+    public AudioSource audioData;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Drumstick")
+        {
+            audioData.Play();
+        }
+    }
+}
