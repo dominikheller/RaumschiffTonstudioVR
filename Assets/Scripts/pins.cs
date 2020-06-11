6a863f3a088958ee03b1e1b2f3c77e46325b330f
malbero <34505168+malbero@users.noreply.github.com>
2020-06-11 13:45:45 +0200

    Split bowlingball and pin scripts

---
+++ Assets/Scripts/pins.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pins : MonoBehaviour
+{
+    Vector3 ballStartingLocation;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballStartingLocation = transform.position;
+        rb = this.gameObject.GetComponent<Rigidbody>();
+    }
+
+    public void resetBowlingBall()
+    {
+        this.gameObject.transform.position = ballStartingLocation;
+        this.gameObject.transform.rotation = Quaternion.identity;
+        rb.velocity = new Vector3(0.0f, 0.0f, 0.0f);
+        rb.angularVelocity = new Vector3(0.0f, 0.0f, 0.0f);
+    }
+}
