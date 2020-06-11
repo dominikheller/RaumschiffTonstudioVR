313d4636290b46c56bf13c4c128a45d935f65ef8
malbero <34505168+malbero@users.noreply.github.com>
2020-06-11 18:22:21 +0200

    Add torch movement sound script

---
+++ Assets/Scripts/Torch.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torch : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public bool isGrabbed;
+    private float velocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isGrabbed == true)
+        {
+            velocity = GetComponent<VelocityIndicator>().velocity;
+            audioSource.volume = .1f + velocity;
+        } else
+        {
+            StartCoroutine(ExecuteAfterTime(.5f));
+        }
+    }
+
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        audioSource.volume = 0;
+    }
+
+    public void setGrabStatusActive()
+    {
+        isGrabbed = true;
+    }
+
+    public void setGrabStatusInactive()
+    {
+        isGrabbed = false;
+    }
+}
