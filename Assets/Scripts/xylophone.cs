7919aefe54dfa8fcf11e683734525f612ef4105a
malbero <34505168+malbero@users.noreply.github.com>
2020-06-11 18:59:54 +0200

    Add velocity script to xylophone in testing scene and add new drum sounds

--- Assets/Scripts/xylophone.cs
+++ Assets/Scripts/xylophone.cs
@@ -4,33 +4,39 @@ using UnityEngine;
 
 public class xylophone : MonoBehaviour
 {
-
     public AudioSource audioData;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-
 
     void OnTriggerEnter(Collider other)
     {
-
         if (other.tag == "playsXylo")
         {
-            audioData.Play(0);
-        }
+            GameObject parentObject = findParentByTag(other.gameObject, "PrimaryGrab");
 
+            if(parentObject)
+            {
+                float velocity = parentObject.GetComponent<VelocityIndicator>().velocity;
+                audioData.volume = velocity;
+                audioData.Play(0);
+            } else
+            {
+                audioData.Play(0);
+            }
+        }
     }
 
+    public static GameObject findParentByTag(GameObject childObject, string tag)
+    {
+        Transform t = childObject.transform;
+        while (t.parent != null)
+        {
+            if (t.parent.tag == tag)
+            {
+                return t.parent.gameObject;
+            }
+            t = t.parent.transform;
+        }
+        return null;
+    }
 }
 
 
