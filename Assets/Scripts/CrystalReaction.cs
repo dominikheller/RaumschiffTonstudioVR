9da75d5d17d8962384b3d6756ab2cb9601dd9eb1
dominikheller <dheller@smail.uni-koeln.de>
2020-06-14 23:27:12 +0200

    made cave crystals reativce to player proximity

---
+++ Assets/Scripts/CrystalReaction.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalReaction : MonoBehaviour
+{
+    private AudioSource audioData;
+    private Animator anim;
+    private bool triggered = false;
+
+
+    void Start()
+    {
+        anim = this.gameObject.GetComponent<Animator>();
+        audioData = this.gameObject.GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "PlayerHeadset")
+        {
+            anim.Play("glow");
+            audioData.Play(0);
+        }
+    }
+
+}
+
+
