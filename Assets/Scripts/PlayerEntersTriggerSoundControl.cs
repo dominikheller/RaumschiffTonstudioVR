907c1e4f53a389b8135722678a455e1f1c61e2c1
malbero <34505168+malbero@users.noreply.github.com>
2020-06-21 18:28:13 +0200

    Add some soundeffect tests to cave scene

---
+++ Assets/Scripts/PlayerEntersTriggerSoundControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEntersTriggerSoundControl : MonoBehaviour
+{
+    public AudioSource audioSource;
+    private bool soundPlayed;
+
+    private void Start()
+    {
+        soundPlayed = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "PlayerHeadset" && soundPlayed == false)
+        {
+            audioSource.Play();
+            soundPlayed = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        soundPlayed = false;
+    }
+}
+
