ce99c261bb4a0fd59235f902294962a31a0715a8
malbero <34505168+malbero@users.noreply.github.com>
2020-07-02 22:12:43 +0200

    Add prototypical baton

--- Assets/Scripts/Taktstock.cs
+++ Assets/Scripts/Taktstock.cs
@@ -1,27 +1,88 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Taktstock : MonoBehaviour
 {
-
     public AudioSource audioSource;
-    public Rigidbody rigidbody;
-    
 
-    // Start is called before the first frame update
-    void Start()
+    private float velocity;
+    private float velocityRangeLevel;
+
+    private void Start()
     {
-        
+        velocityRangeLevel = 0f;
+        //InvokeRepeating("setPitchLevel", 0f, .5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        var audioClipSpeed = 1.0f;
-        var p = rigidbody.velocity.magnitude / audioClipSpeed;
-        audioSource.pitch = Mathf.Clamp(p, 0.0f, 1.3f); // p is clamped to sane values
+        velocity = GetComponent<VelocityIndicator>().velocity;
+        //velocityRangeLevel = getVelocityRangeLevel(velocity);
 
+        audioSource.pitch = velocity + 1;
+        audioSource.outputAudioMixerGroup.audioMixer.SetFloat("Pitch", 1f / (velocity + 1));
+    }
 
+    private void setPitchLevel()
+    {
+        audioSource.pitch = velocityRangeLevel + 1;
+        audioSource.outputAudioMixerGroup.audioMixer.SetFloat("Pitch", 1f / (velocityRangeLevel + 1));
+    }
+
+    private float getVelocityRangeLevel(float velocity)
+    {
+        float velocityRangeLevelLocal = 0f;
+
+        if (velocity < 0.1)
+        {
+            velocityRangeLevelLocal = .1f;
+        }
+
+        else if (velocity < 0.2)
+        {
+            velocityRangeLevelLocal = .2f;
+        }
+
+        else if (velocity < 0.3)
+        {
+            velocityRangeLevelLocal = .3f;
+        }
+
+        else if (velocity < 0.4)
+        {
+            velocityRangeLevelLocal = .4f;
+        }
+
+        else if (velocity < 0.5)
+        {
+            velocityRangeLevelLocal = .5f;
+        }
+
+        else if (velocity < 0.6)
+        {
+            velocityRangeLevelLocal = .6f;
+        }
+
+        else if (velocity < 0.7)
+        {
+            velocityRangeLevelLocal = .7f;
+        }
+
+        else if (velocity < 0.8)
+        {
+            velocityRangeLevelLocal = .8f;
+        }
+
+        else if (velocity < 0.9)
+        {
+            velocityRangeLevelLocal = .9f;
+        }
+
+        else if (velocity < 1f)
+        {
+            velocityRangeLevelLocal = 1f;
+        }
+
+        return velocityRangeLevelLocal;
     }
 }
