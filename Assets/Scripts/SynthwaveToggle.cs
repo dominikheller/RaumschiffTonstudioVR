93fa625c74aa5ed87315386f69cc0d6da4876865
dominikheller <dheller@smail.uni-koeln.de>
2020-07-03 17:06:35 +0200

    fixed button drift, added skybox change, made it toggle

---
+++ Assets/Scripts/SynthwaveToggle.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SynthwaveToggle : MonoBehaviour
+{
+
+    public Material skyboxToChangeTo; 
+    public Material skyboxDefault;
+    public bool synthOn = false;
+    public GameObject synthScape;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void toggleSynthwave()
+    {
+        if (synthOn == false)
+        {
+            RenderSettings.skybox = skyboxToChangeTo;
+            synthScape.SetActive(true);
+            synthOn = true;
+        }
+        
+        else if (synthOn == true)
+        {
+
+            RenderSettings.skybox = skyboxDefault;
+            synthScape.SetActive(false);
+            synthOn = false;
+
+        }
+
+    }
+}
