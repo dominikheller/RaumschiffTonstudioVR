14fb831bdf63565ff76ebe54e2dab896fedfa9ae
dominikheller <dheller@smail.uni-koeln.de>
2020-07-07 21:28:48 +0200

    EchoMix Kippschalter, Launchpad, MixAndMatchSoundscape and much more

---
+++ Assets/Scripts/AudioButtonLoopToggle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AudioButtonLoopToggle: MonoBehaviour
+{
+    
+    public AudioSource audioSource;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+
+    }
+
+    public void toggleAudioLoop()
+    {
+        if (audioSource.isPlaying) {
+
+            audioSource.Pause();
+        }
+
+        else
+        {
+            audioSource.Play();
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/ChangeMaterialWhileAudioIsPlayingOnlyOneObject.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ChangeMaterialWhileAudioIsPlayingOnlyOneObject: MonoBehaviour
+{
+    
+    public GameObject gameObject;
+    public AudioSource audioSource;
+    public Material materialBefore;
+    public Material materialDuring;
+    public Material materialNone;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (audioSource.clip == null)
+        {
+            Renderer rend = gameObject.GetComponent<Renderer>();
+            rend.material = materialNone;
+       
+        }
+
+        else if (audioSource.isPlaying)
+        {
+            Renderer rend = gameObject.GetComponent<Renderer>();
+            rend.material = materialDuring;
+
+        }
+
+        else
+        {
+            Renderer rend = gameObject.GetComponent<Renderer>();
+            rend.material = materialBefore;
+        }
+    }
+}
