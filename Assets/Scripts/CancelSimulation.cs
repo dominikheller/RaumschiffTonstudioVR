0096a4404f32b9955a851cabb1911f650d6e8b70
dominikheller <dheller@smail.uni-koeln.de>
2020-07-12 16:35:09 +0200

    added button and confirmation menu to get back to the spaceship, added toilet taper
    (did a shitty job with the toilet paper, they only have box colliders)

---
+++ Assets/Scripts/CancelSimulation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CancelSimulation : MonoBehaviour
+{
+
+
+    public GameObject confirmPanel;
+    
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "OtherHand")
+        {
+            confirmPanel.SetActive(true);
+        
+        }
+
+
+        }
+
+}
---
+++ Assets/Scripts/CancelSimulationYes.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CancelSimulationYes : MonoBehaviour
+{
+
+
+    public GameObject confirmPanel;
+    
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "OtherHand")
+        {
+            confirmPanel.SetActive(false);
+
+            //change the level back to spaceship script here
+        
+        }
+
+
+        }
+
+}
