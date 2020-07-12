9b98c2fc21fe655da069b965a593d318122672d9
MoritzMeid <57092535+MoritzMeid@users.noreply.github.com>
2020-07-12 18:27:53 +0200

    AAAAAAAAAAAAARGH

---
+++ Assets/Scripts/ReloadAudio.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReloadAudio : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    public GameObject[] audioSources;
+    public float spawnTime;
+
+    Coroutine spawnAudio; 
+
+    void Start()
+    {
+        spawnAudio = StartCoroutine(SpawnAudioSources());
+    }
+
+    IEnumerator SpawnAudioSources()
+    {
+
+        yield return new WaitForSeconds(spawnTime);
+        foreach (GameObject source in audioSources)
+        {
+            source.SetActive(true);
+        }
+    }
+}
